9f02d3667bddc57ea9c2702640d1c5e160a5a259
Amr Monier <a.monier2107@gmail.com>
2021-04-04 12:08:37 +0200

    added Bootstrap5, Axios, Vue-Axios

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "bootstrap";
+import Axios from "axios";
+import VueAxios from "vue-axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 Vue.config.productionTip = false;
 
-new Vue({
+Vue.use(VueAxios, Axios);
+new Vue({ 
   router,
   store,
   render: (h) => h(App),
