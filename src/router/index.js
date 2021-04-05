4a7d68fa7e710b1ae75b3f55dd854cda32e31c4a
Amr Monier <a.monier2107@gmail.com>
2021-04-05 21:35:25 +0200

    setting up the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,25 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-
+import RecipesComponent from "@/components/Recipes.vue";
+import RecipeDetailsComponent from "@/components/RecipeDetails.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  
+  {
+    path: "/",
+    name: "Recipes",
+    component: RecipesComponent,
+  },
+  {
+    path: "/meal/:id",
+    name: "Recipe-details",
+    props: (route) => ({ ...route.params }),
+    component: RecipeDetailsComponent,
+    beforeEnter: (to, from, next) => {
+      Number.isInteger(Number.parseInt(to.params.id)) ? next() : next("/");
+    },
+  },
 ];
 
 const router = new VueRouter({
